745f70e807ba4a2d281661910e9f9b41e5b7821a
Dana <ochoa.dana2001@gmail.com>
2026-02-19 09:16:34 -0600

    update

---
+++ app/Http/Resources/BookResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BookResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'titulo' => $this->titulo,
+            'descripcion' => $this->descripcion,
+            'ISNB' => $this->ISNB,
+            'copias_totales' => $this->copias_totales,
+            'copias_disponibles' => $this->copias_disponibles,
+            'estado' => $this->estado,
+        ]
+    }
+}
