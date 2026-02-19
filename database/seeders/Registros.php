4d9d14f64d1d9b1ee028d646bb5baec9ee9e71e8
Dana <ochoa.dana2001@gmail.com>
2026-02-19 08:30:31 -0600

    update

--- database/seeders/Registros.php
+++ database/seeders/Registros.php
@@ -20,5 +20,41 @@ class Registros extends Seeder
             'copias_disponibles' => 5,
             'estado' => true,
         ]);
+
+        DB::table('books')->insert([
+            'titulo' => 'La Odisea',
+            'descripcion' => 'Viaje épico de Ulises',
+            'ISNB' => 9788424924515,
+            'copias_totales' => 3,
+            'copias_disponibles' => 3,
+            'estado' => true,
+        ]);
+
+        DB::table('books') ->insert([
+            'titulo' => 'El Gran Gatsby',
+            'descripcion' => 'El sueño americano en los años 20',
+            'ISNB' => 9788467036411,
+            'copias_totales' => 4, 
+            'copias_disponibles' => 4,
+            'estado' => true,
+        ])
+
+        DB::table('books') ->insert([
+            'titulo' => 'Rayuela',
+            'descripcion' => 'Novela experimental de Cortázar',
+            'ISNB' => 9788420431321,
+            'copias_totales' => 2,
+            'copias_disponibles' => 2,
+            'estado' => true,
+        ])
+
+        DB::table('books') ->insert([
+            'titulo' => 'Hamlet',
+            'descripcion' => 'Tragedia de venganza y duda',
+            'ISNB' => 9788437600123,
+            'copias_totales' => 5,
+            'copias_disponibles' => 5,
+            'estado' =>true,
+        ])
     }
 }
